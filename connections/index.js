9a22d2146094e759ea231851eb3509577243274c
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-05-07 16:39:52 +0800

    新增 posts API

---
+++ connections/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path:'./config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABAES_PASSWORD
+);
+
+mongoose.connect(DB)
+  .then(() => console.log('資料庫連線成功'))
+  .catch((err) => console.log(err));
